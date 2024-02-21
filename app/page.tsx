1a247d69b35e15a6b9fbac7931462247ac07ba78
Oli Carter <olicartr@gmail.com>
2024-02-21 15:31:30 +0200

    Update layout and number of moons

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,44 @@
 import { clsx } from 'clsx'
 import { Moon } from 'lunarphase-js'
 
-const numMoons = 30
+const numMoons = 10
 const maxDifference = numMoons / 2
 
 export default function Home() {
   return (
-    <main className="bottom-0 fixed flex flex-col items-center justify-center left-0 right-0 top-0">
-      <div className="flex gap-4 items-center justify-center w-full">
-        {new Array(30).fill(null).map((_, i) => {
-          const date = new Date()
-          const difference = i - maxDifference
-          date.setDate(date.getDate() + difference)
-
-          return (
-            <div
-              key={i}
-              className="aspect-square shrink-0"
-              style={{
-                width: `${
-                  ((maxDifference - Math.abs(difference)) / maxDifference) * 10
-                }%`,
-              }}
+    <main>
+      <header className="cursor-default p-4">
+        <a className="text-sm" href="https://moonphase-nu.vercel.app">
+          {'moonphase-nu.vercel.app'.split('').map((letter, index) => (
+            <span
+              className="delay-200 duration-1000 hover:delay-0 hover:duration-0 hover:opacity-100 opacity-30 relative transition-opacity z-10"
+              key={index}
             >
-              <MoonGraphic date={date} />
-            </div>
-          )
-        })}
+              {letter}
+            </span>
+          ))}
+        </a>
+      </header>
+      <div className="bottom-0 fixed flex flex-col items-center justify-center left-0 right-0 top-0">
+        <div className="flex gap-[3vmin] items-center justify-center w-full">
+          {new Array(numMoons).fill(null).map((_, i) => {
+            const date = new Date()
+            const difference = i - maxDifference
+            date.setDate(date.getDate() + difference)
+
+            return (
+              <div
+                key={i}
+                className="aspect-square shrink-0 -translate-x-1/2 w-[33vmin]"
+                style={{
+                  opacity: 1 - Math.abs(difference) / maxDifference,
+                }}
+              >
+                <MoonGraphic date={date} />
+              </div>
+            )
+          })}
+        </div>
       </div>
     </main>
   )

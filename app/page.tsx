98d802b58421bbf6f084952d4c55346017d9a95d
Oli Carter <olicartr@gmail.com>
2024-02-24 00:01:55 +0200

    Add date-fns library and implement date change functionality

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,23 @@
 
 import { clsx } from 'clsx'
 import { Moon } from 'lunarphase-js'
-import { useState } from 'react'
-
-const numMoons = 29
-const maxDifference = Math.ceil(numMoons / 2)
+import { useEffect, useState } from 'react'
+import { addMinutes, format } from 'date-fns'
 
 export default function Home() {
-  const [isLarge, setIsLarge] = useState(true)
-  const width = isLarge
-    ? `calc(${100 * numMoons}vmin - ${32 * numMoons}px)`
-    : '100vw'
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const changeDate = (e: WheelEvent) => {
+      setDate(d => addMinutes(d, e.deltaX + e.deltaY))
+    }
+    window.addEventListener('wheel', changeDate)
+    return () => window.removeEventListener('wheel', changeDate)
+  }, [])
 
   return (
-    <main className="h-svh" onClick={() => setIsLarge(p => !p)}>
-      <header className="cursor-default fixed p-4 top-0 left-0 text-yellow-50/20 text-xs">
+    <main className="h-svh">
+      <header className="cursor-default fixed font-mono p-4 top-0 left-0 text-yellow-50/20 text-xs">
         <h1>
           <a
             className="hover:text-yellow-50"
@@ -24,52 +27,22 @@ export default function Home() {
             target="_blank"
           >
             moonphase
-          </a>{' '}
-          <span>
-            by{' '}
-            <a
-              className="hover:text-yellow-50"
-              href="https://github.com/olicarter"
-              rel="noreferrer"
-              target="_blank"
-            >
-              olicarter
-            </a>
-          </span>
+          </a>
         </h1>
       </header>
       <div className="bottom-0 fixed flex flex-col items-center justify-around left-0 py-[3vmin] right-0 top-0 pointer-events-none">
-        <div
-          className="duration-1000 grid grid-cols-[repeat(29,1fr)] gap-[1vmin] transition-[width]"
-          style={{ width }}
-        >
-          {new Array(numMoons).fill(null).map((_, i) => {
-            const date = new Date()
-            const difference = i - maxDifference
-            date.setDate(date.getDate() + difference)
-
-            return (
-              <div
-                key={i}
-                className="aspect-square shrink-0 -translate-x-[calc(100%+1vmin)] w-full"
-                style={{
-                  opacity:
-                    difference === 0
-                      ? 1
-                      : 0.5 - (Math.abs(difference) / maxDifference) * 0.5,
-                }}
-              >
-                <MoonGraphic date={date} />
-              </div>
-            )
-          })}
+        <div className="absolute font-bold font-mono mix-blend-difference text-xs text-white z-50">
+          {format(date, 'dd.MM.yy HH:mm')}
+        </div>
+        <div className="aspect-square duration-1000 shrink-0 w-[calc(100vmin-32px)] transition-[width]">
+          <MoonGraphic date={date} />
         </div>
       </div>
     </main>
   )
 }
 
-function MoonGraphic({ date = new Date() }: { date?: Date }) {
+function MoonGraphic({ date }: { date: Date }) {
   const lunarAgePercent = Moon.lunarAgePercent(date)
   const phase = (lunarAgePercent * 360 + 180) % 360
 
@@ -80,8 +53,8 @@ function MoonGraphic({ date = new Date() }: { date?: Date }) {
         className={clsx(
           'absolute bg-gradient-to-r from-50% h-full overflow-hidden rounded-full to-50% w-full z-10',
           phase < 90 && 'from-yellow-50 to-yellow-50',
-          phase >= 90 && phase < 180 && 'from-neutral-950 to-neutral-950',
-          phase >= 180 && phase < 270 && 'from-neutral-950 to-neutral-950',
+          phase >= 90 && phase < 180 && 'from-neutral-900 to-neutral-900',
+          phase >= 180 && phase < 270 && 'from-neutral-900 to-neutral-900',
           phase >= 270 && 'from-yellow-50 to-transparent',
         )}
         style={{ transform: `rotateY(${phase}deg)` }}
@@ -89,10 +62,10 @@ function MoonGraphic({ date = new Date() }: { date?: Date }) {
       <div
         className={clsx(
           'absolute bg-gradient-to-r from-50% h-full rounded-full to-50% w-full',
-          phase < 90 && 'from-yellow-50 to-neutral-950',
-          phase >= 90 && phase < 180 && 'from-yellow-50 to-neutral-950',
-          phase >= 180 && phase < 270 && 'from-neutral-950 to-yellow-50',
-          phase >= 270 && 'from-neutral-950 to-yellow-50',
+          phase < 90 && 'from-yellow-50 to-neutral-900',
+          phase >= 90 && phase < 180 && 'from-yellow-50 to-neutral-900',
+          phase >= 180 && phase < 270 && 'from-neutral-900 to-yellow-50',
+          phase >= 270 && 'from-neutral-900 to-yellow-50',
         )}
       />
     </div>
